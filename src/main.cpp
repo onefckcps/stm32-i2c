9503f03a40c666648ed971521df0bd44ea83cf53
Simon <44637374+onefckcps@users.noreply.github.com>
2024-02-22 17:01:10 +0100

    trying even harder

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,60 @@
-#include <Arduino.h>
 #include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define VCNL4200_ADDRESS 0x51 // I2C address for VCNL4200
 
-int slaveAddress;
+uint16_t readALSData();
+
+uint16_t readALSData()
+{
+  // Declare the function readALSData
+
+  uint8_t alsDataLow, alsDataHigh;
+  uint16_t alsData;
+
+  // Start I2C Transmission
+  Wire.beginTransmission(VCNL4200_ADDRESS);
+  // Send ALS Data Low Byte Command
+  Wire.write(0x09);            // ALS Data Low Byte Register Address
+  Wire.endTransmission(false); // End Transmission, but don't release the bus (Repeated Start)
+
+  // Request 1 byte of data
+  Wire.requestFrom(VCNL4200_ADDRESS, 1);
+  // Read the low byte
+  if (Wire.available() == 1)
+  {
+    alsDataLow = Wire.read();
+  }
+
+  // Start I2C Transmission
+  Wire.beginTransmission(VCNL4200_ADDRESS);
+  // Send ALS Data High Byte Command
+  Wire.write(0x0A);            // ALS Data High Byte Register Address
+  Wire.endTransmission(false); // End Transmission, but don't release the bus (Repeated Start)
+
+  // Request 1 byte of data
+  Wire.requestFrom(VCNL4200_ADDRESS, 1);
+  // Read the high byte
+  if (Wire.available() == 1)
+  {
+    alsDataHigh = Wire.read();
+  }
+
+  // Combine the two bytes
+  alsData = (alsDataHigh << 8) | alsDataLow;
+
+  return alsData;
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin(); // Initiate I2C communication
-  slaveAddress = 0x51;
-
-  Wire.beginTransmission(slaveAddress);
-  Wire.requestFrom(address, quantity);
-
-  //   pinMode(D15, OUTPUT);
-  //   digitalWrite(D15, HIGH);
-
-  //   pinMode(D14, OUTPUT);
-  //   digitalWrite(D14, HIGH);
-
-  Wire.endTransmission();
+  Wire.begin();       // Initialize I2C
+  Serial.begin(9600); // Start serial communication for debugging#
 }
-
-// Serial.print("Result: ");
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  // for (int i = 0; i < 1000; i += 10)
-  // {
-  //   Serial.print(i);
-  // }
-  // for (int i = 1000; i > 0; i -= 10)
-  // {
-  //   Serial.print(i);
-  // }
-}
-
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  uint16_t alsData = readALSData();
+  Serial.print("ALS Data: ");
+  Serial.println(alsData);
+  delay(1000); // Wait for 1 second before reading again
 }
